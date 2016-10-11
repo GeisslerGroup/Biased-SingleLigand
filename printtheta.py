8aad697615d28641f4e70370e9a8159bc8c261ba
Pratima Satish <pratimasatish@berkeley.edu>
2016-10-11 10:50:58 -0700

    probably fixed the moving restraint range issue to get bias values correctly

--- printtheta.py
+++ printtheta.py
@@ -4,13 +4,18 @@ import argparse
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-bias", type=float, default=2500.0, help="bias value for spring constant")
-parser.add_argument("-at_lower", type=float, default=1.40, help="lower limit of order parameter in biased simulation")
-parser.add_argument("-at_upper", type=float, default=1.10, help="upper limit of order parameter in biased simulation")
-parser.add_argument("-at_step", type=float, default=-0.05, help="step between each consecutive umbrella")
+parser.add_argument("-lower", type=float, default=1.65, help="lower limit of order parameter in biased simulation")
+parser.add_argument("-upper", type=float, default=1.10, help="upper limit of order parameter in biased simulation")
+parser.add_argument("-step", type=float, default=-0.05, help="step between each consecutive umbrella")
 parser.add_argument("-timestep", type=int, default=5000, help="timesteps after which umbrella is moved")
 args   = parser.parse_args()
 
-at_range = np.arange(args.at_lower, args.at_upper+args.at_step, args.at_step)
+# at_range = np.arange(args.lower, args.upper+args.step, args.step)
+steps = ((args.upper - args.lower) / args.step) + 1.0
+steps = np.ceil(steps)
+at_range = np.zeros(steps)
+for i in np.arange(steps):
+    at_range[i] = args.lower + args.step * i
 
 str_out = ""
 
